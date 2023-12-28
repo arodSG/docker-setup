3154b740f6b000b10af1198db47c13c3c2bcaf25
arodSG <andrewpr52@gmail.com>
2023-12-27 21:00:16 -0600

    changes for 64-bit OS

--- setup.sh
+++ setup.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
+$DOCKER_DIST='raspbian'
+
 # Add Docker's official GPG key:
 sudo apt-get update
 sudo apt-get install -y ca-certificates curl gnupg
-echo y | sudo install -m 0755 -d /etc/apt/keyrings
+sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/raspbian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
+# 64-bit
+if [ $(uname -m) == 'aarch64' ]; then
+    $DOCKER_DIST='debian'
+fi
+
 # Set up Docker's APT repository:
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/raspbian \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/$DOCKER_DIST \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
 sudo apt-get update
 
 # Install docker and mariadb
